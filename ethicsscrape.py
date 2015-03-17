e7300785595a9504f3c17627c78bc939bd377a90
Madi Alexander <madi.alexander@gmail.com>
2015-03-17 14:08:47 -0500

    fixed non-breaking spaces, skips first column in table

---
+++ ethicsscrape.py
@@ -0,0 +1,33 @@
+from BeautifulSoup import BeautifulSoup
+
+html = open('page.html', 'r').read()
+
+
+soup = BeautifulSoup(html)
+lobbyists = soup.find('table', attrs={'id': 'ctl00_ContentPlaceHolder_grvMain'})
+
+
+output = []
+
+# Your scraper should do several things:
+
+# 1. First, grab all the lobbyist expenditures from the table
+# 2. Skip, or ignore, the column that says "View"
+# 3. If the transaction is for an official in Columbia, add it to the output list
+# 4. Remove the &nbsp; string from all output where it appears
+# 5. At the end, print the output list
+
+########## YOUR CODE GOES HERE ##########
+
+
+for tr in lobbyists.findAll('tr'):
+
+    output_row = []
+
+    for td in tr.findAll('td')[1:]:
+        data = td.text.replace('&nbsp;', '')
+        output_row.append(data)
+
+    output.append(output_row)
+
+print output
